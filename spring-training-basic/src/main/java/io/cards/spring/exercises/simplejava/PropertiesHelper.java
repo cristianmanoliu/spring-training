31a0610a756623c14440c575f2b2121dcca76763
tsorin <tsorin@gmail.com>
2017-12-21 15:32:07 +0200

    changes

--- spring-training-basic/src/main/java/io/cards/spring/exercises/simplejava/PropertiesHelper.java
+++
@@ -1,9 +0,0 @@
-package io.cards.spring.exercises.simplejava;
-
-import java.util.ResourceBundle;
-
-public class PropertiesHelper {
-    public static String getProperty(String file, String property) {
-        return ResourceBundle.getBundle(file).getString(property);
-    }
-}
--- spring-training-basic/src/main/java/io/cards/spring/exercises/simplejava/SimpleJava.java
+++ spring-training-basic/src/main/java/io/cards/spring/exercises/simplejava/SimpleJava.java
@@ -3,12 +3,14 @@ package io.cards.spring.exercises.simplejava;
 import io.cards.spring.exercises.simplejava.service.MessagingService;
 import io.cards.spring.exercises.simplejava.service.MessagingServiceFactory;
 
+import java.util.ResourceBundle;
+
 public class SimpleJava {
     public static void main(String[] args) {
-        String messagingType = PropertiesHelper.getProperty("messaging", "messaging.type");
-        MessagingService service = MessagingServiceFactory.createMessagingService(messagingType);
+        String messagingType = ResourceBundle.getBundle("messaging").getString("messaging.type");
 
         for (int i = 0; i < 5; i++) {
+            MessagingService service = MessagingServiceFactory.createMessagingService(messagingType);
             System.out.println(service);
             service.sendMessage("simple java");
         }
