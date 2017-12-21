fb4fec1181169a3e34e1dc02252867125f3e55da
tsorin <tsorin@gmail.com>
2017-12-21 15:39:13 +0200

    changes

--- spring-training-basic/src/main/java/io/cards/spring/exercises/simplejava/SimpleJava.java
+++ spring-training-basic/src/main/java/io/cards/spring/exercises/simplejava/SimpleJava.java
@@ -3,11 +3,14 @@ package io.cards.spring.exercises.simplejava;
 import io.cards.spring.exercises.simplejava.service.MessagingService;
 import io.cards.spring.exercises.simplejava.service.MessagingServiceFactory;
 
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.util.Properties;
 
 public class SimpleJava {
-    public static void main(String[] args) {
-        String messagingType = ResourceBundle.getBundle("messaging").getString("messaging.type");
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        properties.load(SimpleJava.class.getResourceAsStream("/messaging.properties"));
+        String messagingType = properties.getProperty("messaging.type");
 
         for (int i = 0; i < 5; i++) {
             MessagingService service = MessagingServiceFactory.createMessagingService(messagingType);
