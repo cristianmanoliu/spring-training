718007399eb2f1880653606060580917523a5a4d
tsorin <tsorin@gmail.com>
2017-12-21 15:14:23 +0200

    changes

--- spring-training-basic/src/main/java/io/cards/spring/simplejava/SimpleJava.java
+++ spring-training-basic/src/main/java/io/cards/spring/simplejava/SimpleJava.java
@@ -3,10 +3,14 @@ package io.cards.spring.simplejava;
 import io.cards.spring.simplejava.service.MessagingService;
 import io.cards.spring.simplejava.service.MessagingServiceFactory;
 
-public class Bootstrap {
+public class SimpleJava {
     public static void main(String[] args) {
         String messagingType = PropertiesHelper.getProperty("messaging", "messaging.type");
         MessagingService service = MessagingServiceFactory.createMessagingService(messagingType);
-        service.sendMessage("simple java");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(service);
+            service.sendMessage("simple java");
+        }
     }
 }
